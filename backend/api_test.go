257581c8a226846199c06abdd265ccaa59c19f3e
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-09-10 21:38:59 +0300

    add single get test

--- backend/api_test.go
+++ backend/api_test.go
@@ -12,8 +12,8 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestGetSalary(t *testing.T) {
-	Convey("Get Salary", t, func() {
+func TestGetSalaries(t *testing.T) {
+	Convey("Get Salaries", t, func() {
 		repository := GetCleanTestRepository()
 		service := NewService(repository)
 		api := NewApi(&service)
@@ -91,6 +91,67 @@ func TestGetSalary(t *testing.T) {
 				So(actualResult[1].Users[0].Name, ShouldEqual, salary1.Users[0].Name)
 				So(actualResult[1].Users[0].Email, ShouldEqual, salary1.Users[0].Email)
 				//So(actualResult[1].Users[0].UpdatedAt, ShouldEqual, salary.Users[0].UpdatedAt)
+			})
+		})
+	})
+}
+
+func TestGetSalary(t *testing.T) {
+	Convey("Get Salary", t, func() {
+
+		repository := GetCleanTestRepository()
+		service := NewService(repository)
+		api := NewApi(&service)
+
+		salary := models.Salary{
+			ID:        GenerateUUID(8),
+			Salary:    "2000",
+			Debit:     "100",
+			MoneyGain: "200",
+			CreatedAt: time.Now().UTC().Round(time.Second),
+			UpdatedAt: time.Now().UTC().Round(time.Second),
+			Users: []models.User{
+				models.User{
+					ID:        GenerateUUID(8),
+					Name:      "Metehan",
+					Email:     "metehan@gmail.com",
+					CreatedAt: time.Now().UTC().Round(time.Second),
+					UpdatedAt: time.Now().UTC().Round(time.Second),
+				}},
+		}
+
+		repository.CreateSalary(salary)
+
+		Convey("When the get request sent", func() {
+			app := SetupApp(&api)
+			req, _ := http.NewRequest(http.MethodGet, "/", nil)
+			resp, err := app.Test(req, 3000)
+
+			So(err, ShouldBeNil)
+
+			Convey("Then status code should be 200", func() {
+				So(resp.StatusCode, ShouldEqual, fiber.StatusOK)
+			})
+
+			Convey("Then product should be returned", func() {
+				actualResult := models.Salary{}
+				actualRespBody, _ := io.ReadAll(resp.Body)
+				err := json.Unmarshal(actualRespBody, &actualResult)
+
+				So(err, ShouldBeNil)
+
+				So(actualResult.ID, ShouldEqual, salary.ID)
+				So(actualResult.Salary, ShouldEqual, salary.Salary)
+				So(actualResult.Debit, ShouldEqual, salary.Debit)
+				So(actualResult.MoneyGain, ShouldEqual, salary.MoneyGain)
+				So(actualResult.UpdatedAt, ShouldEqual, salary.UpdatedAt)
+				So(actualResult.CreatedAt, ShouldEqual, salary.CreatedAt)
+				So(actualResult.Users[0].ID, ShouldEqual, salary.Users[0].ID)
+				So(actualResult.Users[0].Name, ShouldEqual, salary.Users[0].Name)
+				So(actualResult.Users[0].Email, ShouldEqual, salary.Users[0].Email)
+				So(actualResult.Users[0].CreatedAt, ShouldEqual, salary.Users[0].CreatedAt)
+				So(actualResult.Users[0].UpdatedAt, ShouldEqual, salary.Users[0].UpdatedAt)
+
 			})
 		})
 	})
